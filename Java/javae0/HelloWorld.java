0f2ffbe30dc23c6cd161eb75541c058fb7d84f92
Eswar540 <83905923+Eswar540@users.noreply.github.com>
2021-11-16 20:51:04 +0530

    Delete Java/javae0 directory

--- Java/javae0/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-//import the required packages
-import java.lang.*;
- public class HelloWorld{
-    public static void main(String[] args){
-        System.out.println("Hello World!! Welcome to java");
-    }
-}
--- Java/javae0/State.java
+++
@@ -1,7 +0,0 @@
-//Creating a package
-package Budget;
-
-public interface State{
-    void setRevenue(double Revenue);
-    double getRevenue();
-}
